d33a5ecb4781d652b17b544187eade86f00f2194
Mr-AXEL01 <abdelhak20azur02@gmail.com>
2024-10-16 01:15:13 +0100

    Feat: add embddeds key
    implement embeddeds classes to compose key from two principal key

---
+++ src/main/java/net/axel/domains/embeddeds/GeneralResultKey.java
@@ -0,0 +1,14 @@
+package net.axel.domains.embeddeds;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Embeddable;
+
+import java.io.Serializable;
+import java.util.UUID;
+
+@Embeddable
+public record GeneralResultKey(
+        @Column(name = "cyclist_id") UUID cyclistId,
+        @Column(name = "competition_id") UUID competitionId
+) implements Serializable {
+}
---
+++ src/main/java/net/axel/domains/embeddeds/ResultKey.java
@@ -0,0 +1,14 @@
+package net.axel.domains.embeddeds;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Embeddable;
+
+import java.io.Serializable;
+import java.util.UUID;
+
+@Embeddable
+public record ResultKey(
+        @Column(name = "cyclist_id") UUID cyclistId,
+        @Column(name = "stage_id") UUID stageId
+) implements Serializable {
+}
