a693991efd0b3d2b3fa9ee3ca7b1855ddf69093c
Mr-AXEL01 <abdelhak20azur02@gmail.com>
2024-10-17 14:26:05 +0100

    Feat : test getTeamById()
    implement two test for getTeamById , where the first test to get the
    Team that we are searching for.
    the second test is to get an empty optional when the team that we search
    for it is not exists.

--- src/test/java/net/axel/services/implementations/TeamServiceTest.java
+++ src/test/java/net/axel/services/implementations/TeamServiceTest.java
@@ -2,8 +2,13 @@ package net.axel.services.implementations;
 
 import net.axel.domains.entities.Team;
 import net.axel.repositories.TeamRepository;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.Optional;
 import java.util.UUID;
@@ -11,18 +16,36 @@ import java.util.UUID;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.when;
 
+@ExtendWith(MockitoExtension.class)
 class TeamServiceTest {
 
+    @Mock
+    private TeamRepository teamRepository;
+
+    @InjectMocks
+    private TeamService teamService;
+
+    private Team team;
+
+    @BeforeEach
+    void setUP() {
+        team = new Team()
+                .setId(UUID.randomUUID())
+                .setName("morocco");
+    }
+
     @Test
-    public void testGetTeamById() {
-    TeamRepository teamRepositoryMock = Mockito.mock(TeamRepository.class);
-    UUID teamId = UUID.randomUUID();
-    when(teamRepositoryMock.findById(teamId)).thenReturn(Optional.of(new Team(teamId, "morocco")));
-    TeamService teamService = new TeamService(teamRepositoryMock);
+    public void givenId_whenGetTeamById_thenReturnTeam() {
+        when(teamRepository.findById(team.getId())).thenReturn(Optional.of(team));
+        Team result = teamService.getTeamById(team.getId());
+        assertEquals("morocco", result.getName());
+    }
 
-    Team team = teamService.getTeamById(teamId);
-
-    assertEquals("morocco", team.getName());
+    @Test
+    public void givenId_whenGetTeamById_thenReturnNull() {
+        when(teamRepository.findById(team.getId())).thenReturn(Optional.empty());
+        Team result = teamService.getTeamById(team.getId());
+        assertNull(result);
     }
 
 
