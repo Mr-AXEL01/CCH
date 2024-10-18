fb52195812726a8a6887e5cd248a2b6d6f55ecf2
Mr-AXEL01 <abdelhak20azur02@gmail.com>
2024-10-18 01:26:42 +0100

    Docs : handle some technnincal code to let lombok do it
    update the inject of cyclust repository in a constructor and use
    annotation to let lombok take care of that

--- src/main/java/net/axel/services/implementations/CyclistService.java
+++ src/main/java/net/axel/services/implementations/CyclistService.java
@@ -1,6 +1,7 @@
 package net.axel.services.implementations;
 
 import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
 import net.axel.domains.entities.Cyclist;
 import net.axel.repositories.CyclistRepository;
 import net.axel.services.interfaces.ICyclistService;
@@ -11,20 +12,18 @@ import java.util.UUID;
 
 @Service
 @Transactional
+@RequiredArgsConstructor
 public class CyclistService implements ICyclistService {
 
     private final CyclistRepository cyclistRepository;
-
-    public CyclistService(CyclistRepository cyclistRepository) {
-        this.cyclistRepository = cyclistRepository;
-    }
 
     public List<Cyclist> getAllCyclists() {
         return cyclistRepository.findAll();
     }
 
     public Cyclist getCyclistById(UUID id) {
-        return cyclistRepository.findById(id).orElse(null);
+        return cyclistRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Cyclist not found with id :" + id));
     }
 
     public Cyclist saveCyclist(Cyclist cyclist) {
