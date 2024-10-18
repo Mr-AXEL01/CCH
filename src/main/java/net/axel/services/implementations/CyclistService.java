79fc513f69fd2fcd0f6a99dc917e55e372766044
Mr-AXEL01 <abdelhak20azur02@gmail.com>
2024-10-18 01:44:54 +0100

    Feat : save a Cyclist
    inject  the teamService so we can get team ibject from the team id , and
    use it to crate a Cyclist object to save it.

--- src/main/java/net/axel/services/implementations/CyclistService.java
+++ src/main/java/net/axel/services/implementations/CyclistService.java
@@ -2,9 +2,12 @@ package net.axel.services.implementations;
 
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
+import net.axel.domains.dtos.CyclistDto;
 import net.axel.domains.entities.Cyclist;
+import net.axel.domains.entities.Team;
 import net.axel.repositories.CyclistRepository;
 import net.axel.services.interfaces.ICyclistService;
+import net.axel.services.interfaces.ITeamService;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -16,6 +19,7 @@ import java.util.UUID;
 public class CyclistService implements ICyclistService {
 
     private final CyclistRepository cyclistRepository;
+    private final ITeamService teamService;
 
     public List<Cyclist> getAllCyclists() {
         return cyclistRepository.findAll();
@@ -26,7 +30,9 @@ public class CyclistService implements ICyclistService {
                 .orElseThrow(() -> new RuntimeException("Cyclist not found with id :" + id));
     }
 
-    public Cyclist saveCyclist(Cyclist cyclist) {
+    public Cyclist saveCyclist(CyclistDto dto) {
+        Team team = teamService.getTeamById(dto.teamId());
+        Cyclist cyclist = new Cyclist(dto.firstName(), dto.lastName(), dto.birthdate(), dto.nationality(), team);
         return cyclistRepository.save(cyclist);
     }
 
