7f2aa0c683afc078752080de6e07d5584dc66097
Mr-AXEL01 <abdelhak20azur02@gmail.com>
2024-10-18 22:55:23 +0100

    Feat : set the dto for stage.
    implement the dto request for the stage toget the attribute from the
    controller and create object.

---
+++ src/main/java/net/axel/domains/dtos/StageDto.java
@@ -0,0 +1,29 @@
+package net.axel.domains.dtos;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import net.axel.domains.enums.StageType;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+public record StageDto(
+        @NotNull(message = "number of the stage os required.")
+        int stageNumber,
+
+        @NotBlank(message = "the Start location for the stage is required.")
+        String startLocation,
+
+        @NotBlank(message = "the end location for the stage is required.")
+        String endLocation,
+
+        @NotNull(message = "the start date for the stage is required.")
+        LocalDateTime startDateTime,
+
+        @NotNull(message = "stage type is required")
+        StageType stageType,
+
+        @NotNull(message = "the competition id for the stage is required.")
+        UUID competitionId
+) {
+}
