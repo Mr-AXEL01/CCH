060628d8c4e644d77dcc0055b8f62e362ab3c43e
Mr-AXEL01 <abdelhak20azur02@gmail.com>
2024-10-19 15:37:25 +0100

    Feat : setthe method getStageById()
    implement the get stage by ID , and throw an exception in case the stage
    not found

--- src/main/java/net/axel/services/implementations/StageService.java
+++ src/main/java/net/axel/services/implementations/StageService.java
@@ -1,21 +1,32 @@
 package net.axel.services.implementations;
 
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
 import net.axel.domains.dtos.StageDto;
 import net.axel.domains.entities.Stage;
+import net.axel.repositories.StageRepository;
 import net.axel.services.interfaces.IStageService;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.UUID;
 
+@Service
+@Transactional
+@RequiredArgsConstructor
 public class StageService implements IStageService {
+
+    private final StageRepository stageRepository;
+
     @Override
     public List<Stage> getAllStages() {
-        return List.of();
+        return stageRepository.findAll();
     }
 
     @Override
     public Stage getStageById(UUID id) {
-        return null;
+        return stageRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Error finding stage with id :" +id));
     }
 
     @Override
