48c7fb72a70580a5ed9316a7317321534ec9427d
Mr-AXEL01 <abdelhak20azur02@gmail.com>
2024-10-19 16:16:00 +0100

    Feat : set the save of stage()
    implement the creation of stage by getting the data from stage dto and
    set a stage object with constructir and pass it to the stage repository.

--- src/main/java/net/axel/services/implementations/StageService.java
+++ src/main/java/net/axel/services/implementations/StageService.java
@@ -3,6 +3,7 @@ package net.axel.services.implementations;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import net.axel.domains.dtos.StageDto;
+import net.axel.domains.entities.Competition;
 import net.axel.domains.entities.Stage;
 import net.axel.repositories.StageRepository;
 import net.axel.services.interfaces.IStageService;
@@ -17,6 +18,7 @@ import java.util.UUID;
 public class StageService implements IStageService {
 
     private final StageRepository stageRepository;
+    private final CompetitionService competitionService;
 
     @Override
     public List<Stage> getAllStages() {
@@ -31,7 +33,9 @@ public class StageService implements IStageService {
 
     @Override
     public Stage saveStage(StageDto dto) {
-        return null;
+        Competition competition = competitionService.getCompetitionById(dto.competitionId());
+        Stage stage = new Stage(dto.stageNumber(), dto.startLocation(), dto.endLocation(), dto.startDateTime(), dto.stageType(), competition);
+        return stageRepository.save(stage);
     }
 
     @Override
