bfdab2bd68dcce27e48ff88ed8f65f69d5e323c2
Mr-AXEL01 <abdelhak20azur02@gmail.com>
2024-10-20 18:30:09 +0100

    Feat : set a dto for the Result
    implement a dto for the result to create new object of the result

---
+++ src/main/java/net/axel/domains/dtos/ResultDto.java
@@ -0,0 +1,18 @@
+package net.axel.domains.dtos;
+
+import jakarta.validation.constraints.NotNull;
+
+import java.time.Duration;
+import java.util.UUID;
+
+public record ResultDto(
+        @NotNull(message = "cyclist ID is required for the result record.")
+        UUID cyclistId,
+
+        @NotNull(message = "stage ID is required for the result record.")
+        UUID stageId,
+
+        @NotNull(message = "time recorded for the cyclist for the result.")
+        Duration time
+) {
+}
