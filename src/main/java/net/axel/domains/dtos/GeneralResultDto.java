29ee1a21f70c440a956b106a55ba8ba0420e6d74
Mr-AXEL01 <abdelhak20azur02@gmail.com>
2024-10-20 01:26:51 +0100

    Feat : set dto for the general result
    implement a dto for generalResult to get the id of both the compeittion
    and the cyclist for the subsription.

---
+++ src/main/java/net/axel/domains/dtos/GeneralResultDto.java
@@ -0,0 +1,14 @@
+package net.axel.domains.dtos;
+
+import jakarta.validation.constraints.NotNull;
+
+import java.util.UUID;
+
+public record GeneralResultDto(
+        @NotNull(message = "cyclist ID is required for the sub.")
+        UUID cyclistId,
+
+        @NotNull(message = "Competition ID is required for the cyclist Sub.")
+        UUID competitionId
+) {
+}
