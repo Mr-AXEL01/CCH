47650cb27522f90327eaddd23637708160da6ed2
Mr-AXEL01 <abdelhak20azur02@gmail.com>
2024-10-22 11:00:15 +0100

    Feat : set a dispatcherServlet Config
    implement the config for the main servlet 'dispatcherServlet' that will
    take all the requests and start processing them

---
+++ src/main/java/net/axel/config/DispatcherServletConfig.java
@@ -0,0 +1,23 @@
+package net.axel.config;
+
+import jakarta.servlet.ServletContext;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRegistration;
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+
+public class DispatcherServletConfig implements WebApplicationInitializer {
+
+
+    @Override
+    public void onStartup(ServletContext servletContext) throws ServletException {
+        AnnotationConfigWebApplicationContext webApplicationContext = new AnnotationConfigWebApplicationContext();
+        webApplicationContext.register(WebConfig.class, PersistenceConfig.class);
+
+        DispatcherServlet dispatcherServlet = new DispatcherServlet(webApplicationContext);
+        ServletRegistration.Dynamic dispatcher = servletContext.addServlet("dispatcher", dispatcherServlet);
+        dispatcher.addMapping("/");
+        dispatcher.setLoadOnStartup(1);
+    }
+}
