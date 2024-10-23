88d7a437a7f92fbfcf6e1300d3febf69dbbe65fa
Mr-AXEL01 <abdelhak20azur02@gmail.com>
2024-10-23 14:02:06 +0100

    Feat : set mapper for the Cyclist && Team
    aextnde fromthe base mapper and set inetfaces for cyclist and team.

---
+++ src/main/java/net/axel/mapper/CyclistMapper.java
@@ -0,0 +1,10 @@
+package net.axel.mapper;
+
+import net.axel.domains.dtos.cyclists.CyclistDto;
+import net.axel.domains.dtos.cyclists.CyclistResponseDTO;
+import net.axel.domains.entities.Cyclist;
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "spring")
+public interface CyclistMapper extends BaseMapper<Cyclist, CyclistResponseDTO, CyclistDto>{
+}
---
+++ src/main/java/net/axel/mapper/TeamMapper.java
@@ -0,0 +1,10 @@
+package net.axel.mapper;
+
+import net.axel.domains.dtos.teams.TeamDto;
+import net.axel.domains.dtos.teams.TeamResponseDTO;
+import net.axel.domains.entities.Team;
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "spring")
+public interface TeamMapper extends BaseMapper<Team, TeamResponseDTO, TeamDto>{
+}
