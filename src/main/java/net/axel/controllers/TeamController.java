aa0659784c591be4ce50089a9c62d11e62ef4f18
Mr-AXEL01 <abdelhak20azur02@gmail.com>
2024-10-23 16:03:31 +0100

    Feat : use the ResponeEntity
    implement the ResponseEntity for a better control of the status and data
    we send to the client

--- src/main/java/net/axel/controllers/TeamController.java
+++ src/main/java/net/axel/controllers/TeamController.java
@@ -1,26 +1,28 @@
 package net.axel.controllers;
 
 import lombok.RequiredArgsConstructor;
-import net.axel.domains.dtos.TeamResponseDTO;
+import net.axel.domains.dtos.teams.TeamResponseDTO;
 import net.axel.services.interfaces.ITeamService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping(TeamController.CONTROLLER_PATH)
 @RequiredArgsConstructor
 public class TeamController {
-    public final static String CONTROLLER_PATH = "api/v1/teams";
+    public final static String CONTROLLER_PATH = "/api/v1/teams";
     private final ITeamService teamService;
 
     @GetMapping
-    public List<TeamResponseDTO> getAllTeams() {
-        return teamService.getAllTeams()
-                .stream()
-                .map(team -> new TeamResponseDTO(team.getId(), team.getName()))
-                .toList();
+    public ResponseEntity<List<TeamResponseDTO>> getAllTeams() {
+        List<TeamResponseDTO> teams = teamService.getAllTeams();
+        return ResponseEntity.ok(teams);
     }
+
 }
