b263a897cbd415fe77a30fc7e7027e304f0b42eb
Mr-AXEL01 <abdelhak20azur02@gmail.com>
2024-10-23 16:38:01 +0100

    Feat : add end point for the craetion of new team
    implement the end point of creation of a new team by saving the request
    body 'TeamDto' and return the created team with a status CREATED '201'

--- src/main/java/net/axel/controllers/TeamController.java
+++ src/main/java/net/axel/controllers/TeamController.java
@@ -1,13 +1,12 @@
 package net.axel.controllers;
 
 import lombok.RequiredArgsConstructor;
+import net.axel.domains.dtos.teams.TeamDto;
 import net.axel.domains.dtos.teams.TeamResponseDTO;
 import net.axel.services.interfaces.ITeamService;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.UUID;
@@ -29,5 +28,11 @@ public class TeamController {
     public ResponseEntity<TeamResponseDTO> getTeamById(@PathVariable UUID id) {
         TeamResponseDTO team = teamService.getTeamById(id);
         return ResponseEntity.ok(team);
+    }
+
+    @PostMapping
+    public ResponseEntity<TeamResponseDTO> createTeam(@RequestBody TeamDto dto) {
+        TeamResponseDTO createdTeam = teamService.saveTeam(dto);
+        return new ResponseEntity<>(createdTeam, HttpStatus.CREATED);
     }
 }
