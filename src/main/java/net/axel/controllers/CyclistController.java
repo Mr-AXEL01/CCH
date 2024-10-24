cb7df9c4203505b502ff20db8a501ddd0176510b
Mr-AXEL01 <abdelhak20azur02@gmail.com>
2024-10-24 01:14:42 +0100

    Feat : add the controller for the Cyclist
    implement the cyclistController and the set getAllCyclists(), and return
    the list of cyclist with status 200

---
+++ src/main/java/net/axel/controllers/CyclistController.java
@@ -0,0 +1,25 @@
+package net.axel.controllers;
+
+import lombok.RequiredArgsConstructor;
+import net.axel.domains.dtos.cyclists.CyclistResponseDTO;
+import net.axel.services.interfaces.ICyclistService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(TeamController.CONTROLLER_PATH)
+@RequiredArgsConstructor
+public class CyclistController {
+    public final static String CONTROLLER_PATH = "/api/v1/cyclist";
+    private final ICyclistService cyclistService;
+
+    @GetMapping
+    public ResponseEntity<List<CyclistResponseDTO>> getAllCyclists() {
+        List<CyclistResponseDTO> cyclists = cyclistService.getAllCyclists();
+        return ResponseEntity.ok(cyclists);
+    }
+}
