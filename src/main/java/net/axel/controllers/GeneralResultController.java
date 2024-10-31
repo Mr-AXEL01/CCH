abaff75eb34e33b1a05eb9a0475d9095b7aed6c8
Mr-AXEL01 <abdelhak20azur02@gmail.com>
2024-10-31 11:47:02 +0100

    Feat : set methods to delete && get a specify generalResult
    implement method to get a specify generalResult depend on the competitionId && cyclistId , and the same the deleting , to drespect the endpoint in the chier de charge.

--- src/main/java/net/axel/controllers/GeneralResultController.java
+++ src/main/java/net/axel/controllers/GeneralResultController.java
@@ -5,8 +5,10 @@ import net.axel.domains.dtos.generalResults.GeneralResultResponseDTO;
 import net.axel.domains.embeddeds.GeneralResultKey;
 import net.axel.domains.entities.GeneralResult;
 import net.axel.services.interfaces.IGeneralResultService;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
 
 @RestController
 @RequestMapping(GeneralResultController.CONTROLLER_PATH)
@@ -15,5 +17,28 @@ public class GeneralResultController extends BaseController<GeneralResult, Gener
 
     public GeneralResultController(IGeneralResultService generalResultService) {
         super(generalResultService);
+    }
+
+
+    @GetMapping("/{competitionId}/{cyclistId}")
+    public ResponseEntity<GeneralResultResponseDTO> getByEmbeddedId(
+            @PathVariable("competitionId") UUID competitionId,
+            @PathVariable("cyclistId") UUID cyclistId) {
+
+        GeneralResultKey id = new GeneralResultKey(cyclistId, competitionId);
+
+        GeneralResultResponseDTO result = baseService.getById(id);
+        return ResponseEntity.ok(result);
+    }
+
+    @DeleteMapping("/{competitionId}/{cyclistId}")
+    public ResponseEntity<Void> delete(
+            @PathVariable("competitionId") UUID competitionId,
+            @PathVariable("cyclistId") UUID cyclistId) {
+
+        GeneralResultKey id = new GeneralResultKey(cyclistId, competitionId);
+
+        baseService.delete(id);
+        return ResponseEntity.noContent().build();
     }
 }
